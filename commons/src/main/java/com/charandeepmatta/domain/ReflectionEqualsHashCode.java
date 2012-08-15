311055d2df8472873d27a6581b1673ae90eb72b8
Charandeep S. Matta <charandeep.matta@gmail.com>
2012-08-15 21:39:09 +0300

    reflection based equals hash code

---
+++ commons/src/main/java/com/charandeepmatta/domain/ReflectionEqualsHashCode.java
@@ -0,0 +1,24 @@
+package com.charandeepmatta.domain;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+public class ReflectionEqualsHashCode implements Serializable {
+    @Override
+    public boolean equals(final Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
